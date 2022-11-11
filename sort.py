2a696d183644099956c893614477ee30e023786c
孙丹阳 <sundany@hotmail.com>
2022-11-11 23:06:19 +0800

    排序算法

--- sort.py
+++ sort.py
@@ -19,3 +19,14 @@ def selectSort(lyst):
         if minIndex != i:
             swap(lyst, minIndex, i)
         i += 1
+
+
+def bubblesort(lyst):
+    n = len(lyst)
+    while n > 1:
+        i = 1
+        while i < n:
+            if lyst[i] < lyst[i - 1]:
+                swap(lyst, i, i - 1)
+            i += 1
+        n -= 1
