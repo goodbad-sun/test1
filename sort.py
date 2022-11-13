3dfdd0b47632b4aceb7517702d2741a5b98bc91b
孙丹阳 <sundany@hotmail.com>
2022-11-13 23:26:44 +0800

    排序算法

--- sort.py
+++ sort.py
@@ -30,3 +30,33 @@ def bubblesort(lyst):
                 swap(lyst, i, i - 1)
             i += 1
         n -= 1
+
+
+def bubblesortwithtweak(lyst):
+    n = len(lyst)
+    while n > 1:
+        i = 1
+        while i < n:
+            if lyst[i] < lyst[i - 1]:
+                swap(lyst, i, i - 1)
+                swapped = True
+            i += 1
+        if not swapped:
+            return
+        n -= 1
+
+
+def insertSort(lyst):
+    """插入排序，第i轮找到的最大值应该插入到第i项"""
+    i = 1
+    while i < len(lyst):
+        itemtoinsert = lyst[i]
+        j = i - 1
+        while j >= 0:
+            if itemtoinsert < lyst[j]:
+                lyst[j + 1] = lyst[j]
+                j -= 1
+            else:
+                break
+        lyst[j + 1] = itemtoinsert
+        i += 1
